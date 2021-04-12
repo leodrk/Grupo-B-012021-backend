4c492212bf184f66580908de651fcbfed5da01ad
leodrk <martos.marcelo@gmail.com>
2021-04-11 22:05:49 -0300

    Commit inicial

---
+++ src/main/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/GrupoB012021BackendApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.dessap.grupob012021.GrupoB012021backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoB012021BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoB012021BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/GrupoB012021BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.dessap.grupob012021.GrupoB012021backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoB012021BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
