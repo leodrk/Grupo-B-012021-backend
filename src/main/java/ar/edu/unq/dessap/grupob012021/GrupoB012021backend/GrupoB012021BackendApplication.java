c3709df72feeca1cdea27401c29a4fa1b08c844b
Matata <matjanikow@gmail.com>
2021-06-19 22:49:21 -0300

    Agrego el llamado de cors

--- src/main/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/GrupoB012021BackendApplication.java
+++ src/main/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/GrupoB012021BackendApplication.java
@@ -2,6 +2,7 @@ package ar.edu.unq.dessap.grupob012021.GrupoB012021backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +10,11 @@ import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class GrupoB012021BackendApplication {
@@ -30,8 +36,8 @@ public class GrupoB012021BackendApplication {
 					.antMatchers(HttpMethod.POST, "/login").permitAll()
 					.antMatchers(HttpMethod.POST, "/register").permitAll()
 					.anyRequest().authenticated();
+			http.cors().configurationSource(corsConfigurationSource());
 		}
-
 		@Override
 		public void configure(WebSecurity web) throws Exception {
 			web.ignoring().antMatchers("/v2/api-docs",
@@ -46,5 +52,16 @@ public class GrupoB012021BackendApplication {
 					"/h2-console/**"
 					);
 		}
+	}
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000"));
+		configuration.setAllowedMethods(Arrays.asList("*"));
+		configuration.setAllowCredentials(true);
+		configuration.addAllowedHeader("*");
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 }
