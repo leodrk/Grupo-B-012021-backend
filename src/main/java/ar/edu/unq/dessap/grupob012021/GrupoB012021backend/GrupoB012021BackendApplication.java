49c57e77b1c42d2133a3b38a7ac78df12724f261
leodrk <martos.marcelo@gmail.com>
2021-06-24 02:38:00 -0300

    Issue #48 - Implementada busqueda performante de contenido

--- src/main/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/GrupoB012021BackendApplication.java
+++ src/main/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/GrupoB012021BackendApplication.java
@@ -2,21 +2,10 @@ package ar.edu.unq.dessap.grupob012021.GrupoB012021backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class GrupoB012021BackendApplication {
 
 	public static void main(String[] args) {
@@ -24,45 +13,5 @@ public class GrupoB012021BackendApplication {
 	}
 
 
-	@EnableWebSecurity
-	@Configuration
-	class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.csrf().disable()
-					.addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
-					.authorizeRequests()
-					.antMatchers(HttpMethod.POST, "/login").permitAll()
-					.antMatchers(HttpMethod.POST, "/register").permitAll()
-					.anyRequest().authenticated();
-			http.cors().configurationSource(corsConfigurationSource());
-		}
-		@Override
-		public void configure(WebSecurity web) throws Exception {
-			web.ignoring().antMatchers("/v2/api-docs",
-					"/swagger-resources",
-					"/swagger-resources/**",
-					"/configuration/ui",
-					"/configuration/security",
-					"/swagger-ui.html",
-					"/webjars/**",
-					"/v3/api-docs/**",
-					"/swagger-ui/**",
-					"/h2-console/**"
-					);
-		}
-	}
-	@Bean
-	CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("http://localhost:8000"));
-		configuration.setAllowedOrigins(Arrays.asList("https://grupo-b-012021-frontend.herokuapp.com/"));
-		configuration.setAllowedMethods(Arrays.asList("*"));
-		configuration.setAllowCredentials(true);
-		configuration.addAllowedHeader("*");
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
 }
+
--- src/main/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/controllers/ContentController.java
+++ src/main/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/controllers/ContentController.java
@@ -1,6 +1,7 @@
 package ar.edu.unq.dessap.grupob012021.GrupoB012021backend.controllers;
 
 import ar.edu.unq.dessap.grupob012021.GrupoB012021backend.model.content.Content;
+import ar.edu.unq.dessap.grupob012021.GrupoB012021backend.model.content.ContentDTO;
 import ar.edu.unq.dessap.grupob012021.GrupoB012021backend.model.review.ReviewCriteriaDTO;
 import ar.edu.unq.dessap.grupob012021.GrupoB012021backend.service.ContentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,5 +24,16 @@ public class ContentController {
     @PostMapping(value = "content/findByCriteria")
     public ResponseEntity<List<Content>> findContentByReviews(@RequestBody ReviewCriteriaDTO reviewCriteriaDTO){
         return new ResponseEntity(contentService.findByReviews(reviewCriteriaDTO), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "getInfo/{contentId}")
+    public ResponseEntity<ContentDTO> getContentInfo (@PathVariable(value = "contentId") int contentId){
+        try {
+            ContentDTO contentDTO = contentService.getContentInfo(contentId);
+            return new ResponseEntity<>(contentDTO,HttpStatus.OK);
+        }
+        catch (Exception e){
+            return new ResponseEntity("Ha ocurrido un error al obtener la información del contenido solicitado",HttpStatus.BAD_REQUEST);
+        }
     }
 }
