425b41ac9d9e31925820a5d8440418c15e7d76f8
Matata <matjanikow@gmail.com>
2021-06-29 04:23:46 -0300

    Creo test de arquitectura sobre interfaces

---
+++ src/test/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/archunit/interfaces/InterfaceRulesTest.java
@@ -0,0 +1,26 @@
+package ar.edu.unq.dessap.grupob012021.GrupoB012021backend.archunit.interfaces;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+public class InterfaceRulesTest {
+    private final JavaClasses classes = new ClassFileImporter().importPackages("ar.edu.unq.dessap.grupob012021.GrupoB012021backend");
+
+    @Test
+    public void interfaces_should_not_have_names_ending_with_the_word_interface() {
+        noClasses().that().areInterfaces().should().haveNameMatching(".*Interface").check(classes);
+    }
+
+    @Test
+    public void interfaces_should_not_have_simple_class_names_containing_the_word_interface() {
+        noClasses().that().areInterfaces().should().haveSimpleNameContaining("Interface").check(classes);
+    }
+
+    @Test
+    public void interfaces_must_not_be_placed_in_implementation_packages() {
+        noClasses().that().resideInAPackage("..impl..").should().beInterfaces().check(classes);
+    }
+}
