bf96f64286d9e2d88a1a7e3b774b04bcde17235d
Matata <matjanikow@gmail.com>
2021-06-29 04:06:53 -0300

    Agrego archUnit al proyecto y examino controllers

---
+++ src/test/java/ar/edu/unq/dessap/grupob012021/GrupoB012021backend/archunit/controllers/ControllerRulesTest.java
@@ -0,0 +1,34 @@
+package ar.edu.unq.dessap.grupob012021.GrupoB012021backend.archunit.controllers;
+import static com.tngtech.archunit.core.domain.JavaClass.Functions.GET_PACKAGE_NAME;
+import com.tngtech.archunit.base.DescribedPredicate;
+import com.tngtech.archunit.base.PackageMatchers;
+import com.tngtech.archunit.core.domain.JavaClass;
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.domain.JavaMember;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.junit.Test;
+import org.springframework.security.access.annotation.Secured;
+
+import static com.tngtech.archunit.core.domain.JavaMember.Predicates.declaredIn;
+import static com.tngtech.archunit.core.domain.properties.CanBeAnnotated.Predicates.annotatedWith;
+import static com.tngtech.archunit.lang.conditions.ArchPredicates.are;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.library.dependencies.SlicesRuleDefinition.slices;
+
+public class ControllerRulesTest {
+    private final JavaClasses classes = new ClassFileImporter().importPackages("ar.edu.unq.dessap.grupob012021.GrupoB012021backend");
+
+    @Test
+    public void controllers_should_only_call_secured_methods() {
+//        classes()
+//                .that().haveNameMatching(".*Controller")
+//                .should().onlyCallMethodsThat(areDeclaredInController())
+//                .check(classes);
+    }
+
+    private DescribedPredicate<JavaMember> areDeclaredInController() {
+        DescribedPredicate<JavaClass> aPackageController = GET_PACKAGE_NAME.is(PackageMatchers.of("..controller..", "java.."))
+                .as("a package '..controller..'");
+        return are(declaredIn(aPackageController));
+    }
+}
